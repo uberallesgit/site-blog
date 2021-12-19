9ec1044fad26f34fd17bb9faf0441aeeed83d7ae
Uber <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>
2021-12-19 01:02:46 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300),nullable=False)
+    text = db.Column(db.Text(300), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
+
+
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/create-article', methods=['POST', 'GET'])
+def create_article():
+    if request.method == 'POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        article = Article(title=title, intro=intro, text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "An error has occurred during article adding)"
+
+    else:
+        return render_template("create-article.html")
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
